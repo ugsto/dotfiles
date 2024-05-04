1abf6cb90047bee3a27f16853d1adc44beb3bebe
André Bortoli <andre@bortoli.phd>
2024-05-04 13:45:48 -0300

    feat: include colorizer + yuck support

---
+++ assets/nvim/lua/plugins/plugin_manifest/05-colorizer.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+table.insert(M, {
+  "norcalli/nvim-colorizer.lua",
+  config = function()
+    require("colorizer").setup()
+  end
+})
+
+return M
---
+++ assets/nvim/lua/plugins/plugin_manifest/05-yuck.lua
@@ -0,0 +1,5 @@
+local M = {}
+
+table.insert(M, { "elkowar/yuck.vim" })
+
+return M
