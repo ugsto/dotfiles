0fc0e92def8d699ca2908f809024d476ab291990
André Bortoli <andre@bortoli.phd>
2024-05-06 10:25:54 -0300

    fix: include log level in nvim-notify

--- assets/nvim/lua/plugins/plugin_manifest/05-nvim-notify.lua
+++ assets/nvim/lua/plugins/plugin_manifest/05-nvim-notify.lua
@@ -1,5 +1,12 @@
 local M = {}
 
-table.insert(M, "rcarriga/nvim-notify")
+table.insert(M, {
+  "rcarriga/nvim-notify",
+  config = function()
+    vim.notify = require("notify").setup({
+      level = 3,
+    })
+  end,
+})
 
 return M
