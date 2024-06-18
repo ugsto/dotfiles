29865ec76ca61e6963d54085922910977f816fec
André Bortoli <andre@bortoli.phd>
2024-06-18 20:48:36 -0300

    fix: ignore shadow warnings (they are annoying when shadowing `err`)

--- modules/nvim/lua/lsp-handlers/gopls.lua
+++ modules/nvim/lua/lsp-handlers/gopls.lua
@@ -1,5 +1,13 @@
 return function()
 	local cfg = require("go.lsp").config()
 
+	cfg.settings = {
+		gopls = {
+			analyses = {
+				shadow = false,
+			},
+		},
+	}
+
 	require("lspconfig").gopls.setup(cfg)
 end
