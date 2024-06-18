e95bc5274e332bda85e2f006f85b87060d3427fd
André Bortoli <andre@bortoli.phd>
2024-06-18 20:48:03 -0300

    fix: treesitter setup

--- modules/nvim/lua/plugins/plugin_manifest/tool_treesitter.lua
+++ modules/nvim/lua/plugins/plugin_manifest/tool_treesitter.lua
@@ -7,11 +7,13 @@ return {
 		},
 	},
 	build = ":TSUpdate",
-	opts = {
-		ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline" },
-		auto_install = true,
-		highlight = {
-			enable = true,
-		},
-	},
+	config = function()
+		require("nvim-treesitter.configs").setup({
+			ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "markdown", "markdown_inline", "go", "gotmpl" },
+			auto_install = true,
+			highlight = {
+				enable = true,
+			},
+		})
+	end,
 }
