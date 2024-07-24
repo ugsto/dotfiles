8ad20620c94606db3bce1906c8c6d15f05cc7bce
André Bortoli <andre@bortoli.phd>
2024-07-24 10:51:24 -0300

    fix: update which keys definition

--- modules/nvim/lua/mappings/user.lua
+++ modules/nvim/lua/mappings/user.lua
@@ -8,90 +8,170 @@ local dap = safe_require("dap")
 local neotest = safe_require("neotest")
 
 M.setup = function(wk)
-	wk.register({
-		e = {
+	wk.add({
+		{
+			"<leader>e",
 			tfm and tfm.open,
-			"Open TFM",
+			desc = "Open TFM",
 		},
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		q = { "<CMD>q<CR>", "Quit" },
-		w = { "<CMD>w<CR>", "Save" },
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		f = {
+		{
+			"<leader>q",
+			"<CMD>q<CR>",
+			desc = "Quit",
+		},
+		{
+			"<leader>w",
+			"<CMD>w<CR>",
+			desc = "Save",
+		},
+		{
+			"<leader>f",
+			nil,
 			name = "Telescope",
-			f = {
-				telescope_builtin and telescope_builtin.find_files,
-				"Find File",
-			},
-			g = {
-				telescope_builtin and telescope_builtin.live_grep,
-				"Grep",
-			},
-			b = {
-				telescope_builtin and telescope_builtin.buffers,
-				"Buffers",
-			},
-			h = {
-				telescope_builtin and telescope_builtin.help_tags,
-				"Help",
-			},
-			c = {
-				telescope_builtin and telescope_builtin.colorscheme,
-				"Colorscheme",
-			},
 		},
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		d = {
+		{
+			"<leader>ff",
+			telescope_builtin and telescope_builtin.find_files,
+			desc = "Find File",
+		},
+		{
+			"<leader>fg",
+			telescope_builtin and telescope_builtin.live_grep,
+			desc = "Grep",
+		},
+		{
+			"<leader>fb",
+			telescope_builtin and telescope_builtin.buffers,
+			desc = "Buffers",
+		},
+		{
+			"<leader>fh",
+			telescope_builtin and telescope_builtin.help_tags,
+			desc = "Help",
+		},
+		{
+			"<leader>fc",
+			telescope_builtin and telescope_builtin.colorscheme,
+			desc = "Colorscheme",
+		},
+		{
+			"<leader>d",
+			nil,
 			name = "Debugger",
-			t = { dap and dap.toggle_breakpoint, "Toggle breakpoint" },
-			b = { dap and dap.toggle_repl, "Toggle repl" },
-			c = { dap and dap.continue, "Continue" },
-			n = { dap and dap.step_over, "Step over" },
-			i = { dap and dap.step_into, "Step into" },
-			o = { dap and dap.step_out, "Step out" },
 		},
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		t = {
+		{
+			"<leader>dt",
+			dap and dap.toggle_breakpoint,
+			desc = "Toggle breakpoint",
+		},
+		{
+			"<leader>db",
+			dap and dap.toggle_repl,
+			desc = "Toggle repl",
+		},
+		{
+			"<leader>dc",
+			dap and dap.continue,
+			desc = "Continue",
+		},
+		{
+			"<leader>dn",
+			dap and dap.step_over,
+			desc = "Step over",
+		},
+		{
+			"<leader>di",
+			dap and dap.step_into,
+			desc = "Step into",
+		},
+		{
+			"<leader>do",
+			dap and dap.step_out,
+			desc = "Step out",
+		},
+		{
+			"<leader>t",
+			nil,
 			name = "Test",
-			r = { neotest and neotest.run.run, "Run" },
-			d = { neotest and function()
-				neotest.run.run({ strategy = "dap" })
-			end, "Run in debug mode" },
-			s = { neotest and neotest.run.stop, "Stop" },
-			a = { neotest and neotest.run.attach, "Attach" },
 		},
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		lg = { "<CMD>LazyGit<CR>", "Open LazyGit" },
-	}, { prefix = "<leader>" })
-
-	wk.register({
-		["<C-h>"] = { "<CMD>wincmd h<CR>" },
-		["<C-j>"] = { "<CMD>wincmd j<CR>" },
-		["<C-k>"] = { "<CMD>wincmd k<CR>" },
-		["<C-l>"] = { "<CMD>wincmd l<CR>" },
-	})
-
-	wk.register({
-		["<A-h>"] = { "<CMD>BufferPrevious<CR>", "Go to previous buffer" },
-		["<A-l>"] = { "<CMD>BufferNext<CR>", "Go to next buffer" },
-
-		["<A-S-H>"] = { "<CMD>BufferMovePrevious<CR>", "Move buffer to previous position" },
-		["<A-S-L>"] = { "<CMD>BufferMoveNext<CR>", "Move buffer to next position" },
-
-		["<A-[>"] = { "<CMD>BufferFirst<CR>", "Go to first buffer" },
-		["<A-]>"] = { "<CMD>BufferLast<CR>", "Go to last buffer" },
-
-		["<A-q>"] = { "<CMD>BufferClose<CR>", "Close buffer" },
+		{
+			"<leader>tr",
+			neotest and neotest.run.run,
+			desc = "Run",
+		},
+		{
+			"<leader>td",
+			neotest and function()
+				neotest.run.run({ strategy = "dap" })
+			end,
+			desc = "Run in debug mode",
+		},
+		{
+			"<leader>ts",
+			neotest and neotest.run.stop,
+			desc = "Stop",
+		},
+		{
+			"<leader>ta",
+			neotest and neotest.run.attach,
+			desc = "Attach",
+		},
+		{
+			"<leader>lg",
+			"<CMD>LazyGit<CR>",
+			desc = "Open LazyGit",
+		},
+		{
+			"<C-h>",
+			"<CMD>wincmd h<CR>",
+		},
+		{
+			"<C-j>",
+			"<CMD>wincmd j<CR>",
+		},
+		{
+			"<C-k>",
+			"<CMD>wincmd k<CR>",
+		},
+		{
+			"<C-l>",
+			"<CMD>wincmd l<CR>",
+		},
+		{
+			"<A-h>",
+			"<CMD>BufferPrevious<CR>",
+			desc = "Go to previous buffer",
+		},
+		{
+			"<A-l>",
+			"<CMD>BufferNext<CR>",
+			desc = "Go to next buffer",
+		},
+		{
+			"<A-S-H>",
+			"<CMD>BufferMovePrevious<CR>",
+			desc = "Move buffer to previous position",
+		},
+		{
+			"<A-S-L>",
+			"<CMD>BufferMoveNext<CR>",
+			desc = "Move buffer to next position",
+		},
+		{
+			"<A-[>",
+			"<CMD>BufferFirst<CR>",
+			desc = "Go to first buffer",
+		},
+		{
+			"<A-]>",
+			"<CMD>BufferLast<CR>",
+			desc = "Go to last buffer",
+		},
+		{
+			"<A-q>",
+			"<CMD>BufferClose<CR>",
+			desc = "Close buffer",
+		},
 	})
 
 	vim.api.nvim_create_autocmd("LspAttach", {
@@ -103,34 +183,34 @@ M.setup = function(wk)
 			local prefixedOpts = { prefix = "<leader>" }
 			vim.tbl_deep_extend("force", prefixedOpts, opts)
 
-			wk.register({
-				g = {
-					name = "Go",
-					d = { vim.lsp.buf.definition, "Go to definition" },
-					i = { vim.lsp.buf.implementation, "Go to implementation" },
-					r = { vim.lsp.buf.references, "Go to references" },
-				},
-				K = { vim.lsp.buf.hover, "Show hover" },
-				["<C-S-k>"] = { vim.lsp.buf.signature_help, "Show signature help" },
+			wk.add({
+				{ "g", name = "Go" },
+				{ "gd", vim.lsp.buf.definition, desc = "Go to definition" },
+				{ "gi", vim.lsp.buf.implementation, desc = "Go to implementation" },
+				{ "gr", vim.lsp.buf.references, desc = "Go to references" },
+				{ "K", vim.lsp.buf.hover, desc = "Show hover" },
+				{ "<C-S-k>", vim.lsp.buf.signature_help, desc = "Show signature help" },
 			}, opts)
 
-			wk.register({
-				["wa"] = { vim.lsp.buf.add_workspace_folder, "Add workspace folder" },
-				["wr"] = { vim.lsp.buf.remove_workspace_folder, "Remove workspace folder" },
-				["wl"] = {
+			wk.add({
+				{ "<leader>wa", vim.lsp.buf.add_workspace_folder, desc = "Add workspace folder" },
+				{ "<leader>wr", vim.lsp.buf.remove_workspace_folder, desc = "Remove workspace folder" },
+				{
+					"<leader>wl",
 					function()
 						print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 					end,
-					"List workspace folders",
+					desc = "List workspace folders",
 				},
-				["D"] = { vim.lsp.buf.type_definition, "Type definition" },
-				["rn"] = { vim.lsp.buf.rename, "Rename" },
-				["ca"] = { vim.lsp.buf.code_action, "Code action" },
-				["F"] = {
+				{ "<leader>D", vim.lsp.buf.type_definition, desc = "Type definition" },
+				{ "<leader>rn", vim.lsp.buf.rename, desc = "Rename" },
+				{ "<leader>ca", vim.lsp.buf.code_action, desc = "Code action" },
+				{
+					"<leader>F",
 					function()
 						vim.lsp.buf.format({ async = true })
 					end,
-					"Format",
+					desc = "Format",
 				},
 			}, prefixedOpts)
 		end,
