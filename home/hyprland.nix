a3fe5ae1a20ad78679db6a29da31e8648a29e98e
André Augusto Bortoli <andre@bortoli.phd>
2025-06-11 07:48:12 -0300

    fix: remove browser auto start

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -91,7 +91,6 @@ in
       ];
 
       exec-once = [
-        "[workspace 1 silent] ${browser}"
         "[workspace 2 silent] ${terminal}"
         "[workspace 4 silent] ${mail}"
         "[workspace 9 silent] ${password-manager}"
