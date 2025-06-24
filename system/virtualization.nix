aa201f16c400314419acaa69d380a867fde6ea1f
André Augusto Bortoli <andre@bortoli.phd>
2025-06-24 03:55:55 -0300

    fix: remove virtualbox

--- system/virtualization.nix
+++ system/virtualization.nix
@@ -4,15 +4,7 @@
     pkg:
     builtins.elem (lib.getName pkg) [
       "Oracle_VirtualBox_Extension_Pack"
-      "zoom-us"
     ];
-
-  virtualisation.virtualbox.host = {
-    enable = true;
-    enableExtensionPack = true;
-  };
-  users.extraGroups.vboxusers.members = [ username ];
-  boot.kernelParams = [ "kvm.enable_virt_at_load=0" ];
 
   virtualisation.docker = {
     enable = true;
