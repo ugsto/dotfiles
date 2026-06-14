abecd3bdc1c302c170ec7c2463173674b37703b1
André Augusto Bortoli <andre@bortoli.phd>
2026-06-14 13:47:54 -0300

    chore: implement pre-commit hooks and perform initial cleanup
    - Added .pre-commit-config.yaml with security and nix-specific hooks
    - Integrated pre-commit into flake.nix devShell
    - Generated .secrets.baseline for secret detection
    - Applied initial formatting and dead code removal

--- pkgs/nvim/config/modules/plugin/utils/flash.nix
+++ pkgs/nvim/config/modules/plugin/utils/flash.nix
@@ -10,13 +10,21 @@
   };
   keymaps = [
     {
-      mode = [ "n" "x" "o" ];
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
       key = "s";
       action = "<cmd>lua require('flash').jump()<cr>";
       options.desc = "Flash";
     }
     {
-      mode = [ "n" "x" "o" ];
+      mode = [
+        "n"
+        "x"
+        "o"
+      ];
       key = "S";
       action = "<cmd>lua require('flash').treesitter()<cr>";
       options.desc = "Flash Treesitter";
