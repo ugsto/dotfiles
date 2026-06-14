798dd5aa90a6ae6fa764c5b94f15cb8eb907dcc8
André Augusto Bortoli <andre@bortoli.phd>
2026-06-14 13:32:08 -0300

    refactor: remove GNOME desktop and related configuration

--- home/personal.nix
+++ home/personal.nix
@@ -13,9 +13,27 @@
 
   home.packages = [
     pkgs.polkit_gnome
-    pkgs.bambu-studio
+  ];
+
+  services.flatpak.enable = true;
+  services.flatpak.packages = [
+    "com.bambulab.BambuStudio"
+    "io.github.tobagin.karere"
+  ];
+  services.flatpak.remotes = [
+    {
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }
   ];
 
   home.username = username;
   home.homeDirectory = "/home/${username}";
+
+  home.sessionVariables = {
+    XDG_DATA_DIRS = "$XDG_DATA_DIRS:$HOME/.local/share/flatpak/exports/share:/var/lib/flatpak/exports/share";
+    GTK_IM_MODULE = "cedilla";
+    QT_IM_MODULE = "cedilla";
+    IBUS_ENABLE_SYNC_MODE = "1";
+  };
 }
