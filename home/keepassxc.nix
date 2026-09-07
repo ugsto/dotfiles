f361444591371b9b103178acf3d9404891cc9c4b
Andre Augusto Bortoli <andre@bortoli.phd>
2026-09-07 15:49:30 -0300

    feat(keepassxc): configure GUI settings and helium browser integration

--- home/keepassxc.nix
+++ home/keepassxc.nix
@@ -1,4 +1,4 @@
-{ pkgs-unstable, ... }:
+{ config, pkgs-unstable, ... }:
 {
   programs.keepassxc = {
     enable = true;
@@ -8,8 +8,20 @@
       Browser = {
         Enabled = true;
         UpdateBinaryPath = false;
+        CustomBrowserType = 1;
+        CustomBrowserLocation = "${config.home.homeDirectory}/.config/net.imput.helium/NativeMessagingHosts";
+        UseCustomBrowser = true;
       };
       FdoSecrets.Enabled = true;
+      GUI = {
+        AdvancedSettings = true;
+        ApplicationTheme = "dark";
+        CompactMode = true;
+        HidePasswords = true;
+      };
     };
   };
+
+  xdg.configFile."net.imput.helium/NativeMessagingHosts/org.keepassxc.keepassxc_browser.json".source =
+    "${pkgs-unstable.keepassxc}/etc/chromium/native-messaging-hosts/org.keepassxc.keepassxc_browser.json";
 }
